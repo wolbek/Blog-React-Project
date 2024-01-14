001c3c83f4d06b4816beff88babb25a01c1d0393
Aakash Vishwakarma <aakashvish22@gmail.com>
2024-01-14 20:23:49 +0530

    Reading Blogs done. Starting with edit blog feature.

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,14 +1,18 @@
 import './BlogCard.css';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import EditIcon from '@mui/icons-material/Edit';
+import { Link } from 'react-router-dom';
 
 function BlogCard(props){
     return (
         <div class='blog-card'>
-            <h3>{props.blog.title}</h3>
-            <h5>{props.blog.author}</h5>
-            <h6>{props.blog.timestamp}</h6>
-            <p>{props.blog.content}</p>
+            <Link to={`/blog/${props.blog.id}`} state={{blog:props.blog}}>
+                <h3>{props.blog.title}</h3>
+                <h5>{props.blog.author}</h5>
+                <h6>{props.blog.timestamp}</h6>
+                <p>{props.blog.content}</p>
+            </Link>
+            
             <button><EditIcon/></button>
             <button onClick={() => {props.removeBlogHandler(props.blog.id)}}><DeleteForeverIcon/></button>
         </div>
--- src/pages/BlogDetail.js
+++ src/pages/BlogDetail.js
@@ -1,9 +1,18 @@
-import './BlogDetail.css'
+import './BlogDetail.css';
+import { Link, useLocation } from 'react-router-dom';
+
 
 function BlogDetail(){
+    const location = useLocation();
+    
     return (
         <>
-        
+            <h1>{location.state.blog.title}</h1>
+            <h3>{location.state.blog.author}</h3>
+            <h4>{location.state.blog.timestamp}</h4>
+            <p>{location.state.blog.content}</p>
+
+            <Link to='/'><button type='button'>Back to blog list</button></Link>
         </>
     );
 }
